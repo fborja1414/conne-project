d262ff9f1d50b1645a54b3f436fd7b254b851d21
Francisco Borja <fborja1414@gmail.com>
2021-08-09 15:26:33 -0700

    init

---
+++ backend/strapi-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2c4dc88bb378e945487e5e27c39e2713'),
+    },
+  },
+});
---
+++ backend/strapi-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ab484814-35f3-4b63-a680-0ec8165a3733'
+};
